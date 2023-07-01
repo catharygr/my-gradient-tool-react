941001176e0707858960cceb27a29752429ee44c
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-01 14:44:45 +0100

    Trabajando en el componente GradientGenerador

--- src/GradientGenerator.jsx
+++ src/GradientGenerator.jsx
@@ -1,22 +1,36 @@
+import { useState } from "react";
+
 export default function GradientGenerator() {
   const [colors, setColors] = useState([]);
 
-  function handleColorChange(color) {
-    setColor1(color);
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Generador de gradientes</h1>
 
-        <div className="color-picker">
+
+  function handleColorChange(index, color) {
+    const newColors = [...colors];
+    newColors[index] = color;
+    setColors(newColors);
+
+  }
+
+  function generateGradient() {
+    const gradient = `linear-gradient(to right, ${colors.join(', ')})`;
+    
+    return (
+      <div>
+        <h2>Generador de gradientes</h2>
+        {[...Array(5)].map((_, index) => (
           <input
+            key={index}
             type="color"
-            value={colors[0]}
-            onChange={(e) => handleColorChange(e.target.value)}
+            value={colors[index] || ''}
+            onChange={(e) => handleColorChange(index, e.target.value)}
           />
-        </div>
-      </header>
-    </div>
-  );
-}
+        ))}
+        <button onClick={generateGradient}>Generar</button>
+      </div>
+    );}
+      
+
+
+
+ 
